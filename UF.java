f94ebfb310aff7afbcb88f566ccf5eca6f3dd9a2
Brian R Reed <b.r.reed@gmail.com>
2021-07-25 19:16:08 -0700

    created UF file with start of connect and find methods
    Signed-off-by: Brian R Reed <b.r.reed@gmail.com>

--- UF.java
+++ UF.java
@@ -1,5 +1,21 @@
-public class UF {
-    public static void main(String[] args) {
-        
+
+public class UF 
+{
+    private int[] id;
+
+    public UF(int N) 
+    {
+        System.out.println(id);
+    }
+
+    public static void connect(int i, int j) {
+        System.out.println("connect");
+
+
+    }
+
+    public static void find(int i) {
+        System.out.println("find");
+
     }
 }
